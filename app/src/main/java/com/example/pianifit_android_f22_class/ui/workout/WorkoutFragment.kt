b5c38d34f0b79910baa48eb5b3a4321d205ea687
fpellicano <fpellicano@newhaven.edu>
2022-10-27 19:15:57 -0400

    Refactored template names to better fit my app

--- app/src/main/java/com/example/pianifit_android_f22_class/ui/workout/WorkoutFragment.kt
+++ app/src/main/java/com/example/pianifit_android_f22_class/ui/workout/WorkoutFragment.kt
@@ -1,4 +1,4 @@
-package com.example.pianifit_android_f22_class.ui.home
+package com.example.pianifit_android_f22_class.ui.workout
 
 import android.content.ContentValues.TAG
 import android.os.Bundle
@@ -6,17 +6,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.pianifit_android_f22_class.databinding.FragmentHomeBinding
-import com.example.pianifit_android_f22_class.ui.plan.PlanFragment
+import com.example.pianifit_android_f22_class.databinding.FragmentWorkoutBinding
 
-class HomeFragment : Fragment() {
+class WorkoutFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentWorkoutBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,17 +26,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(WorkoutViewModel::class.java)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentWorkoutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
 
         binding.btnStartPlan.setOnClickListener {
             Log.d(TAG, "New Plan Selected")
-            val action = HomeFragmentDirections.actionNavigationHomeToPlanFragment()
+            val action = WorkoutFragmentDirections.actionNavigationHomeToPlanFragment()
             findNavController().navigate(action)
         }
 
--- app/src/main/java/com/example/pianifit_android_f22_class/ui/workout/WorkoutViewModel.kt
+++ app/src/main/java/com/example/pianifit_android_f22_class/ui/workout/WorkoutViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.pianifit_android_f22_class.ui.home
+package com.example.pianifit_android_f22_class.ui.workout
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class WorkoutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Workout Fragment"
     }
     val text: LiveData<String> = _text
 }
